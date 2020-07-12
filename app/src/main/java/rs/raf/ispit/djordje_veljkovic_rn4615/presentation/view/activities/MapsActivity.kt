748979f5a2cad3db95424e7f61be24b728283002
dzo <djveljkovic15@raf.rs>
2020-07-12 19:05:08 +0200

    Jos samo estetika

--- app/src/main/java/rs/raf/ispit/djordje_veljkovic_rn4615/presentation/view/activities/MapsActivity.kt
+++ app/src/main/java/rs/raf/ispit/djordje_veljkovic_rn4615/presentation/view/activities/MapsActivity.kt
@@ -2,13 +2,13 @@ package rs.raf.ispit.djordje_veljkovic_rn4615.presentation.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 import rs.raf.ispit.djordje_veljkovic_rn4615.R
+import rs.raf.ispit.djordje_veljkovic_rn4615.data.models.weather.Weather
+import timber.log.Timber
 
 class MapsActivity : AppCompatActivity(R.layout.activity_maps), OnMapReadyCallback {
 
@@ -24,7 +24,9 @@ class MapsActivity : AppCompatActivity(R.layout.activity_maps), OnMapReadyCallba
 
     private fun init() {
         initActionBar()
+
         initMap()
+
     }
 
     private fun initMap() {
@@ -58,9 +60,35 @@ class MapsActivity : AppCompatActivity(R.layout.activity_maps), OnMapReadyCallba
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        val weather: Weather?
+
+        intent.let {
+            weather = it.getParcelableExtra(MainActivity.MESSAGE_KEY)
+
+            activityMapsInfoCity.text = weather.city + ", " + weather.date
+            activityMapsInfoMaxTemp.text = "MAXIMUM DAILY TEMPERATURE: " +weather.max_temp
+            activityMapsInfoMinTemp.text = "MINIMUM DAILY TEMPERATURE: "+weather.min_temp
+            activityMapsInfoWind.text = "WIND SPEED: "+ weather.wind
+            activityMapsInfoUV.text = "UV RADIATION: "+weather.uv
+
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            mMap.uiSettings.isZoomControlsEnabled= true
+
+
+
+            mMap.addMarker(MarkerOptions().position(
+                LatLng(weather.latitude.toDouble(),weather.longitude.toDouble())).title("Marker in "+weather.city))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(
+                LatLng(weather.latitude.toDouble(),weather.longitude.toDouble())))
+
+            mMap.setMinZoomPreference(7f)
+
+        }
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
